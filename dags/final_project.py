c5d298904e1bf5c769694fe9343aa73c250962c0
Nolan Mellott <51956573+NolanMellott@users.noreply.github.com>
2026-09-08 05:01:18 -0400

    Update final_project.py

--- dags/final_project.py
+++ dags/final_project.py
@@ -2,14 +2,22 @@ from datetime import datetime, timedelta
 import pendulum
 import os
 from airflow.decorators import dag
-from airflow.operators.dummy import DummyOperator
-from operators import (StageToRedshiftOperator, LoadFactOperator,
-                       LoadDimensionOperator, DataQualityOperator)
-from helpers import SqlQueries
+from airflow.operators.dummy_operator import DummyOperator
+from final_project_operators.stage_redshift import StageToRedshiftOperator
+from final_project_operators.load_fact import LoadFactOperator
+from final_project_operators.load_dimension import LoadDimensionOperator
+from final_project_operators.data_quality import DataQualityOperator
+from udacity.common import final_project_sql_statements
+
+final_project_sql_statements.SqlQueries.song_
 
 default_args = {
     'owner': 'udacity',
     'start_date': pendulum.now(),
+    'retries':3,
+    'retry_delay' : timedelta(minutes=5),
+    'catchup': False,
+    'email_on_retry': False,
 }
 
 @dag(
@@ -53,4 +61,23 @@ def final_project():
         task_id='Run_data_quality_checks',
     )
 
+    end_operator = DummyOperator(task_id='Stop_execution')
+
+    start_operator >> stage_events_to_redshift
+    start_operator >> stage_songs_to_redshift
+
+    stage_events_to_redshift >> load_songplays_table
+    stage_songs_to_redshift >> load_songplays_table
+
+    load_songplays_table >> load_user_dimension_table
+    load_songplays_table >> load_song_dimension_table
+    load_songplays_table >> load_artist_dimension_table
+    load_songplays_table >> load_time_dimension_table
+
+    load_user_dimension_table >> run_quality_checks
+    load_song_dimension_table >> run_quality_checks
+    load_artist_dimension_table >> run_quality_checks
+    load_time_dimension_table >> run_quality_checks
+
+    run_quality_checks >> end_operator
 final_project_dag = final_project()
